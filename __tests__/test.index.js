f084bc06fb43c73a33ab530d5fdf257d1574c2bd
hoholabs <johnpaulhoran@gmail.com>
2022-04-04 21:58:35 -0400

    starting player tests

--- __tests__/test.index.js
+++ __tests__/test.index.js
@@ -1,5 +1,106 @@
-const { shipFactory } = require('../src/index')
+const { shipFactory, gameboardFactory, playerFactory } = require('../src/index')
 
-test('ship is created with length', () => {
-    expect(shipFactory(2)).toStrictEqual({length:2});
+
+describe.skip('ship stuff', () => {
+
+    let ship2 
+
+    beforeAll(() => {
+        ship2 = shipFactory('patrol', 2);
     });
+
+    test('Check size of ship2', () => {
+        expect(ship2.size).toBe(2);
+    });
+
+    test('Hit ship2, in position 0', () => {
+        expect(ship2.hit(0)).toStrictEqual([1, 0]);
+    });
+
+    test('check if ship2 is sunk // false', () => {
+        expect(ship2.isSunk()).toBe(false);
+    });
+
+    test('Hit ship2, in position 1', () => {
+        expect(ship2.hit(1)).toStrictEqual([1, 1]);
+    });
+
+    test('check if ship2 is sunk // true', () => {
+        expect(ship2.isSunk()).toBe(true);
+    });
+
+
+});
+
+describe.skip('gameboard', () => {
+
+    let board
+
+    beforeAll(() => {
+        board = gameboardFactory();
+        board.placeShip('patrol',2,0,0,0)
+    });
+
+    test('shoot at 0,0', () => {
+        expect(board.receiveAttack([0,0])).toStrictEqual([true,false]);
+    });
+
+    test('check hits', () => {
+        expect(board.hits).toStrictEqual([[0,0]]);
+    });
+
+    test('shoot at 7,7', () => {
+        expect(board.receiveAttack([7,7])).toBe(false);
+    });
+
+    test('check misses', () => {
+        expect(board.misses).toStrictEqual([[7,7]]);
+    });
+
+    test('check for gameOver', () => {
+        expect(board.gameOver()).toBe(false);
+    });
+
+    test('shoot at 0,1', () => {
+        expect(board.receiveAttack([1,0])).toStrictEqual([true,true]);
+    });
+
+    test('check hits', () => {
+        expect(board.hits).toStrictEqual([[0,0],[1,0]]);
+    });
+
+    test('check for gameOver', () => {
+        expect(board.gameOver()).toBe(true);
+    });
+
+});
+
+describe('player things', ()=>{
+
+    let player;
+    let computer;
+
+    beforeAll(() => {
+        player = playerFactory('Hoho');
+        player.gameBoard.placeShip('patrol',2,0,0,0);
+        computer = playerFactory('computer');
+        computer.gameBoard.placeShip('patrol',2,0,0,0);
+    });
+
+    test('player testing players name', () =>{
+        expect(player.name).toBe('Hoho');
+    });
+
+    test('check for gameOver', () => {
+        expect(player.gameBoard.gameOver()).toBe(false);
+    });
+
+    test('player can recieve attacks',()=>{
+        expect (player.gameBoard.receiveAttack([0,0])).toStrictEqual([true,false]);
+    });
+
+    test('player attacks computer at 0,0', () =>{
+        expect(player.attack(computer,[0,0])).toBe(true);
+    });
+
+});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,133 @@
 import './style.css';
 
-const shipFactory = (length) => {
-    return {length};
-}
+const shipFactory = (name, size) => {
 
-const boardFactory = (name, age) => {
-    const sayHello = () => console.log('hello!');
-    return { name, age, sayHello };
+  let shipState = [];
+
+  for (let index = 0; index < size; index++) {
+    shipState.push(0);
+  };
+    
+  const hit = (i) =>{
+    shipState[i] = 1;
+    return shipState;
   };
 
- // module.exports = shipFactory;
+  const isSunk = () =>{
+    for (let index = 0; index < shipState.length; index++) {
+      if(shipState[index] == 0){ 
+        return false;
+      };
+    };
+  
+    return true;
+  };
+
+  return {name, size, hit, isSunk, shipState};
+};
+
+const gameboardFactory = () => {
+  // let axes = { 
+  //   "x": ['0','1','2','3','4','5','6','7','8','9'],
+  //   "y":   ['0','1','2','3','4','5','6','7','8','9']
+  // };
+
+  // for (let i = 0; i < (axes.x.length*axes.y.length); i++) {
+  //   let tile = [Math.floor(i/10),(i%10),0];
+  //   board.push(tile)
+  // };
+
+  // let board = [];
+  let ships = [];
+  let hits = [];
+  let misses = [];
+  // let shipCoords = [];
+
+  const placeShip = (name,l,x,y,o) =>{
+    
+    // orientation, 0=horizontal, 1=vertical
+    let ship = shipFactory(name,l);
+    ship.pos = [];
+    
+    // add the ship coordinates to the ship
+    for (let index = 0; index < l; index++) {
+     
+      if(o == 0){ //if horizontal
+        ship.pos.push([x+index,y])
+
+      } else { //if vertical
+        ship.pos.push([x,y+index])
+      };
+    };
+    ships.push(ship);
+
+
+    // //add the ship's positions to the shipCoords
+    // for (let index = 0; index < l; index++) {
+    //   if(o=='x'){//if horizontal
+    //     shipCoords.push([name,index,x+index,y]) 
+    //   } else {//if vertical
+    //     shipCoords.push([name,index,x,y+index]) 
+    //   }
+    // };
+
+
+  };
+
+  const receiveAttack = ([x,y]) =>{
+
+    //look at each ship in the list
+    for(let index = 0; index < ships.length; index++){
+
+      let currentShip = ships[index];
+
+      //look at each pos coordinate of this ship
+      for(let index = 0; index<currentShip.pos.length; index++){
+
+        let currentCoord = currentShip.pos[index];
+        //if it hits
+        if (currentCoord[0] == x && currentCoord[1] == y){
+          hits.push([x,y]);
+          currentShip.hit(index);
+          return [true,currentShip.isSunk()];
+        };
+      };
+    };
+    //if it misses
+    misses.push([x,y]);
+    return false;
+
+  };
+
+  const gameOver = () =>{
+    for(let index = 0; index<ships.length;i++){
+      if(ships[index].isSunk() == false){
+        return false;
+      } else{
+        return true;
+      }
+    };
+  }
+
+  return {placeShip, receiveAttack, gameOver, hits, misses};
+};
+
+const playerFactory = (name) => {
+
+  const gameBoard = gameboardFactory();
+
+  const attack = (target, coords) =>{
+
+    target.gameBoard.receiveAttack(coords);
+
+  }
+
+  return {name, gameBoard, attack};
+
+}
 
   module.exports = {
-    shipFactory
+    shipFactory,
+    gameboardFactory,
+    playerFactory
 };
